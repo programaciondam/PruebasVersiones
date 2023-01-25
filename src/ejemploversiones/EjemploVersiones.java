4858311b369702516997cc7fa3b577921a050a56
dam <dam@AULA005-PORTATIL1>
2023-01-25 11:10:36 +0100

    funciona

--- src/ejemploversiones/EjemploVersiones.java
+++ src/ejemploversiones/EjemploVersiones.java
@@ -16,6 +16,7 @@ public class EjemploVersiones {
     public static void main(String[] args) {
         System.out.println("Miercoles 25");
         System.out.println("importe priemera etiqueta");
+        System.out.println("otra etiqueta");
     }
     
 }
